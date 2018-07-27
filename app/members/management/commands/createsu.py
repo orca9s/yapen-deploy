72c7fe9ed72604045eb3cc4ad270d5f8eca65ba8
sangmin <sang93423@gmail.com>
2018-07-27 21:39:35 +0900

    deploy시 Health check문제 해결

--- app/members/management/commands/createsu.py
+++ app/members/management/commands/createsu.py
@@ -5,12 +5,14 @@ from django.conf import settings
 from django.contrib.auth import get_user_model
 from django.core.management import BaseCommand
 
+
+
 User = get_user_model()
 
 
 class Command(BaseCommand):
 	def handle(self, *args, **options):
-		secrets = json.loads(open(os.path.join(settings.SECRETS_DIR, 'base.json')))
+		secrets = json.load(open(os.path.join(settings.SECRETS_DIR, 'base.json')))
 		if not User.objects.filter(username=secrets['SUPERUSER_USERNAME']).exists():
 			User.objects.create_superuser(
 				username=secrets['SUPERUSER_USERNAME'],
