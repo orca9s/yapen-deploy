97871eb1fa66d7ae2335a04373ba3400f3d8ae68
sangmin <sang93423@gmail.com>
2018-07-28 02:18:51 +0900

    배포script를 dockerfile을 dockerhub에 저장한것에 맞춰 수정

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,7 @@
 #!/usr/bin/env bash
-
-pipenv lock --requirements > requirements.txt
-
-git add -f .secrets/ requirements.txt
-echo "파일 추가"
+git add -A
+git add -f .secrets/
+echo "requirements 추가"
 eb deploy --profile jsm-eb-user --staged
 
 git reset HEAD .secrets/ requirements.txt
